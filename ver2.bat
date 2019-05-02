88a09df66fb107a8ba08abf691441e9ef203c673
chris <robot@christiandresler.dk>
2019-05-02 22:43:33 +0200

    Create ver2.bat
    Uncertain about how much remains on Chrome on Windows after script is run. Will test at a later date.

---
+++ ver2.bat
@@ -0,0 +1,22 @@
+echo Deleting user data
+
+:: deleting usr data on win
+del "%USERPROFILE%\Desktop\*.*" /F /Q
+del "%USERPROFILE%\Downloads\*.*" /F /Q
+del "%USERPROFILE%\Documents\*.*" /F /Q
+del "%USERPROFILE%\Pictures\*.*" /F /Q
+del "%USERPROFILE%\Videos\*.*" /F /Q
+
+:: delete all usrnames and passwds in the windows credentials manager
+cmdkey.exe /list > "%TEMP%\List.txt"
+findstr.exe Target "%TEMP%\List.txt" > "%TEMP%\tokensonly.txt"
+FOR /F "tokens=1,2 delims= " %%G IN (%TEMP%\tokensonly.txt) DO cmdkey.exe /delete:%%H
+del "%TEMP%\List.txt" /s /f /q
+del "%TEMP%\tokensonly.txt" /s /f /q
+
+:: create shortcut to chrome usr dir
+set ChromeDir=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data
+
+:: delete history files and cache
+del /q /s /f "%ChromeDir%\default\History*.*"
+del /q /s /f "%ChromeDir%\default\Cache\*.*"
